0cb5da4b0e848dc085d525c2c191afea3fc17a35
Pedro <pedro.phdois@gmail.com>
2024-09-25 19:00:45 -0300

    cron

--- server.js
+++ server.js
@@ -28,3 +28,13 @@ fastify.listen({ port: 3001 }, (err) => {
     process.exit(1);
   }
 });
+
+var cron = require('node-cron');
+
+cron.schedule('*/10 * * * *', async () =>  {
+  const client = new PocketBase(POCKET_BASE_URL);
+  await client
+    .collection("users")
+    .authWithPassword("admin@email.com", "Q!W@e3r4");
+  await client.collection("test").create({ time: new Date().toString() });
+});
