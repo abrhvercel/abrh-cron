5772703de379ca5a67e762f495fda676fce00c08
abrhvercel <abrhvercel@gmail.com>
2025-03-12 13:55:39 -0300

    wip

--- server.js
+++ server.js
@@ -1,46 +1,43 @@
 import Fastify from "fastify";
-// import { checkTransactions } from "./transactions.js";
-// import { runNotas } from "./cron/notas/index.js";
-// import dotenv from 'dotenv';
-// import cron from 'node-cron';
+import { checkTransactions } from "./transactions.js";
+import { runNotas } from "./cron/notas/index.js";
+import dotenv from 'dotenv';
+import cron from 'node-cron';
 
-// dotenv.config();
+dotenv.config();
 
-// String.prototype.parseJSON = function () {
-//   try {
-//     return JSON.parse(this);
-//   } catch (error) {
-//     throw new Error("Invalid JSON string");
-//   }
-// };
+String.prototype.parseJSON = function () {
+  try {
+    return JSON.parse(this);
+  } catch (error) {
+    throw new Error("Invalid JSON string");
+  }
+};
 
 const fastify = Fastify({
   logger: true,
 });
 
 fastify.get("/", async function handler(request, reply) {
-  // const response = await runNotas();
-  // reply.send(response);
-  console.log('oi')
-  reply.send(true)
+  const response = await runNotas();
+  reply.send(response);
 });
 
-fastify.listen({ port: 3033, host: "0.0.0.0" }, (err) => {
+fastify.listen({ port: 3001, host: "0.0.0.0" }, (err) => {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
   }
 });
 
-// fastify.listen({ port: 3033, host: "0.0.0.0" }, (err) => {
-//   if (err) {
-//     fastify.log.error(err);
-//     process.exit(1);
-//   }
-// });
+// TODO DIA as 08h e 16h
+cron.schedule('*/1 * * * *', async () =>  {
+  console.log(new Date().toISOString())
+});
 
-// // TODO DIA as 08h e 16h
-// cron.schedule('0 8,16 * * *', async () =>  {
-//   const response = await runNotas();
-//   console.log(response)
-// });
+
+// TODO DIA as 08h e 16h
+cron.schedule('0 8,16 * * *', async () =>  {
+  const response = await runNotas();
+  console.log(response)
+});
