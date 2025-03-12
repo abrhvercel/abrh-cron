99e5ab1b54982df53b61f2d6b5cf6f14d35bed57
abrhvercel <abrhvercel@gmail.com>
2025-03-12 13:44:50 -0300

    disable cron

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ fastify.listen({ port: 3001, host: "0.0.0.0" }, (err) => {
 });
 
 // TODO DIA as 08h e 16h
-cron.schedule('0 8,16 * * *', async () =>  {
-  const response = await runNotas();
-  console.log(response)
-});
+// cron.schedule('0 8,16 * * *', async () =>  {
+//   const response = await runNotas();
+//   console.log(response)
+// });
